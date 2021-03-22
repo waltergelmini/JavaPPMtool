c1d437abeda8bb9c46cc2e4a73e09a2ef3686be6
waltergelmini <walter.gelmini@pgmd.it>
2021-03-22 14:56:09 +0100

    first commit

---
+++ src/main/java/it/pgmd/ppttooljava/PpttooljavaApplication.java
@@ -0,0 +1,13 @@
+package it.pgmd.ppttooljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpttooljavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpttooljavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/pgmd/ppttooljava/PpttooljavaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pgmd.ppttooljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpttooljavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
